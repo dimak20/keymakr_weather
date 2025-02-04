9646dda653a374c824d85beb5e00d402096e6057
Dmytro Kolchak <dima.kolhac@gmail.com>
2025-02-04 16:15:35 +0200

    Add weather view

--- weather_app/views.py
+++ weather_app/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+import uuid
 
-# Create your views here.
+from drf_spectacular.utils import extend_schema
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from weather_app.serializers import CityListSerializer
+
+
+class WeatherView(APIView):
+    @extend_schema(
+        request=CityListSerializer,
+        responses={202: {"task_id": str}},
+        description="Fetch a list of cities and returns task_id to check task status "
+    )
+    def post(self, request):
+        serializer = CityListSerializer(data=request.data)
+
+        if not serializer.is_valid():
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        cities = serializer.validated_data["cities"]
+        task_id = str(uuid.uuid4())
+        fetch_weather_data.apply_async(args=[cities, task_id])
+
+        return Response({"task_id": task_id}, status=status.HTTP_202_ACCEPTED)
