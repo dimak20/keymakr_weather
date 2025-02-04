48f49f95acce201ed8bee75ca5b5a9e17eccae81
Dmytro Kolchak <dima.kolhac@gmail.com>
2025-02-04 19:40:21 +0200

    Fix temperature validator

--- weather_app/validators.py
+++ weather_app/validators.py
@@ -1,4 +1,23 @@
-class WeatherDataValidator:
-    @staticmethod
-    def is_valid_temperature(temperature: float) -> bool:
+from weather_app.interfaces import DataValidator
+
+class TemperatureValidator(DataValidator):
+    def validate(self, data: dict) -> bool:
+        temperature = data.get("temperature")
+
         return -50 <= temperature <= 50
+
+
+class WeatherValidator:
+    validators = [
+        TemperatureValidator
+    ]
+
+    def __init__(self):
+        self._validators = [validator() for validator in self.validators]
+
+    def validate(self, data: dict) -> bool:
+        for validator in self._validators:
+            if not validator.validate(data=data):
+                return False
+
+        return True
