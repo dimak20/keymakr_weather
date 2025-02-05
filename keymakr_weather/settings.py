8284cd7204cb154b1e0be1c3488550a8df2f2794
Dmytro Kolchak <dima.kolhac@gmail.com>
2025-02-05 19:39:13 +0200

    Replace django secret key to .env

--- keymakr_weather/settings.py
+++ keymakr_weather/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-md0xnab*laj+azbne_xu(gbibtml!q-d6tp10ob5lt9%jv+sli"
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-md0xnab*laj")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
