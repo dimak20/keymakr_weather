2e61aed5e6da09a2bedc566bbfafbd480528c59e
Dmytro Kolchak <dima.kolhac@gmail.com>
2025-02-05 16:55:57 +0200

    Add django-extensions to project

--- keymakr_weather/settings.py
+++ keymakr_weather/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     "django_celery_beat",
     "drf_spectacular",
     "drf_spectacular_sidecar",
+    "django_extensions",
     # my apps
     "management_utils",
     "weather_app"
